ed889d082dfe3e5993b34943ac2f907ef85961a2
Steve Montalvo <sjmontalvo@fullsail.edu>
2018-06-13 16:53:03 -0400

    Utility Method

---
+++ MontalvoSteve_Ex6/MontalvoSteve_Ex6/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontalvoSteve_Ex6
+{
+    class Utility
+    {
+        public static void PauseBeforeContinuing()
+        {
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+        }
+    }
+}
