d54a50518425d05b71741d3be295683360d5b24c
Steve Montalvo <sjmontalvo@fullsail.edu>
2018-06-13 16:59:30 -0400

    UserValidation Method

---
+++ MontalvoSteve_Ex6/MontalvoSteve_Ex6/UserValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontalvoSteve_Ex6
+{
+    class UserValidation
+    {
+        public static string IsNullOrWhiteSpace(string usersName)
+        {
+            while (string.IsNullOrWhiteSpace(usersName))
+            {
+                Console.WriteLine("Please select one of the menu options: ");
+                usersName = Console.ReadLine();
+            }
+            return usersName;
+        }
+    }
+}
